4a9a61c34a5cc12ba81b717aad55beef3b716004
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-07-28 00:56:14 -0700

    Setup moneta yaml store. sweet

--- lib/paywatch.rb
+++ lib/paywatch.rb
@@ -1,7 +1,18 @@
 require "paywatch/version"
 require 'etc'
+require 'moneta'
 
 module Paywatch
+  def self.store
+    @store ||= begin
+      path = File.join("#{self.home}", "#{self.env}")
+      FileUtils.mkdir_p path
+      yaml_file = File.join path, 'paywatch.yaml'
+      FileUtils.touch yaml_file
+      Moneta.new :YAML, :file => yaml_file
+    end
+  end
+
   def self.env
     ENV['RACK_ENV'] ||= 'production'
   end
